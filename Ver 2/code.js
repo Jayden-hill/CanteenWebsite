394a09e1b257eaf844034183da22f8ea24df137a
Jayden-hill <99438438+Jayden-hill@users.noreply.github.com>
2023-03-30 10:41:41 +1300

    Add files via upload

--- Ver 2/code.js
+++ Ver 2/code.js
@@ -1,3 +1,29 @@
-function addToOrder (){
-    alert("");
+list=[]
+
+var burger = {
+    number:0,
+    price:10,
+    addItem:function(){
+        this.number ++;
+        alert (this.number);
+        alert ("$" + this.cost());
+    },
+    removeItem:function(){
+        if (this.number > 0){
+            this.number --;
+            alert (this.number);
+            alert ("$" + this.cost());
+        } else {
+
+        }
+    },
+    cost:function(){
+        return this.number*this.price;
+    }
+}
+
+function addToOrder (item){
+    alert(item);
+    list.push(item)
+    alert(list)
 }
